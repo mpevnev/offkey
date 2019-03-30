40da4fb5acc314eda3105146f5f1cbaed537431d
Michail Pevnev <mpevnev@gmail.com>
2019-03-31 01:35:33 +0300

    Massive upgrade
    It even kinda works

--- src/input.rs
+++ src/input.rs
@@ -1,31 +1,25 @@
 use advanced_collections::circular_buffer::CircularBuffer;
 use alsa::pcm::{Format, IO, PCM};
-use nix::errno::Errno;
 use rustfft::num_complex::Complex;
-use rustfft::num_traits::{Num, NumCast};
+use rustfft::num_traits::Num;
 
-use Source::*;
+use crate::normalize::OmniNormal;
+
+use IOBuf::*;
 
 /* ---------- helper macros ---------- */
 
 macro_rules! read_more {
-    ($io:ident, $scratch:ident, $buf:ident) => {{
-        $scratch.resize_with($buf.capacity(), Default::default);
-        dbg!($scratch.len());
-        dbg!($scratch.capacity());
-        match $io.readi($scratch) {
-            Err(err) if err.errno() != Some(Errno::EAGAIN) => return Err(err),
-            _ => { },
-        };
+    ($io:ident, $scratch:ident, $buftype:ident, $buf:ident) => {{
+        let read = $io.readi($scratch)?;
         $buf.extend(
             $scratch
-                .iter()
-                .cloned()
-                .map(NumCast::from)
-                .map(Option::unwrap),
-        );
-        dbg!($buf.len());
-        dbg!($buf.capacity());
+            .iter()
+            .take(read)
+            .cloned()
+            .map(T::from_unnormalized)
+            .map(|r| Complex::new(r, T::zero()))
+            );
         Ok(())
     }};
 }
@@ -33,11 +27,11 @@ macro_rules! read_more {
 /* ---------- main things ---------- */
 
 pub struct Input<'a, T> {
-    source: Source<'a>,
+    source: IOBuf<'a>,
     buf: CircularBuffer<Complex<T>>,
 }
 
-pub enum Source<'a> {
+pub enum IOBuf<'a> {
     I8(IO<'a, i8>, Vec<i8>),
     U8(IO<'a, u8>, Vec<u8>),
     I16(IO<'a, i16>, Vec<i16>),
@@ -49,46 +43,54 @@ pub enum Source<'a> {
 }
 
 impl<'a, T: Default> Input<'a, T> {
-    pub fn from_pcm(pcm: &'a PCM, bufsize: usize) -> alsa::Result<Self> {
+    pub fn from_pcm(pcm: &'a PCM, buffer_millis: usize) -> alsa::Result<Self> {
+        use std::iter::repeat_with;
         let params = pcm.hw_params_current()?;
-        let bufsize = bufsize * (params.get_channels()? as usize);
+        let period_size = params.get_period_size()?.max(1) as usize;
+        let num_channels = params.get_channels()?.max(1) as usize;
+        let scratchsize = 64 * period_size * num_channels;
         let src = match params.get_format()? {
-            Format::S8 => I8(pcm.io_i8()?, Vec::new()),
-            Format::U8 => U8(pcm.io_u8()?, Vec::new()),
-            Format::S16LE | Format::S16BE => I16(pcm.io_i16()?, Vec::new()),
-            Format::U16LE | Format::U16BE => U16(pcm.io_u16()?, Vec::new()),
-            Format::S32LE | Format::S32BE => I32(pcm.io_i32()?, Vec::new()),
-            Format::U32LE | Format::U32BE => U32(pcm.io_u32()?, Vec::new()),
-            Format::FloatLE | Format::FloatBE => F32(pcm.io_f32()?, Vec::new()),
-            Format::Float64LE | Format::Float64BE => F64(pcm.io_f64()?, Vec::new()),
+            Format::S8 => I8(pcm.io_i8()?, vec![0; scratchsize]),
+            Format::U8 => U8(pcm.io_u8()?, vec![0; scratchsize]),
+            Format::S16LE | Format::S16BE => I16(pcm.io_i16()?, vec![0; scratchsize]),
+            Format::U16LE | Format::U16BE => U16(pcm.io_u16()?, vec![0; scratchsize]),
+            Format::S32LE | Format::S32BE => I32(pcm.io_i32()?, vec![0; scratchsize]),
+            Format::U32LE | Format::U32BE => U32(pcm.io_u32()?, vec![0; scratchsize]),
+            Format::FloatLE | Format::FloatBE => F32(pcm.io_f32()?, vec![0.0; scratchsize]),
+            Format::Float64LE | Format::Float64BE => {
+                F64(pcm.io_f64()?, vec![0.0; scratchsize])
+            }
             _ => return Err(alsa::Error::unsupported("Unsupported sample format")),
         };
+        let rate = params.get_rate()?.max(1) as usize;
+        let mut buf = repeat_with(Complex::default)
+            .take(2 * rate * buffer_millis / 1000 * num_channels)
+            .collect::<CircularBuffer<_>>();
+        buf.resize(buf.capacity());
         Ok(Input {
             source: src,
-            buf: CircularBuffer::new(bufsize),
+            buf,
         })
     }
 }
 
-impl<'a> Source<'a> {
-    pub fn read<T: NumCast>(
-        &mut self,
-        buf: &mut CircularBuffer<T>,
-    ) -> alsa::Result<()> {
+impl<'a> IOBuf<'a> {
+    pub fn read<T>(&mut self, buf: &mut CircularBuffer<Complex<T>>) -> alsa::Result<()>
+        where T: OmniNormal + Num + Clone  {
         match self {
-            I8(io, scratch) => read_more!(io, scratch, buf),
-            U8(io, scratch) => read_more!(io, scratch, buf),
-            I16(io, scratch) => read_more!(io, scratch, buf),
-            U16(io, scratch) => read_more!(io, scratch, buf),
-            I32(io, scratch) => read_more!(io, scratch, buf),
-            U32(io, scratch) => read_more!(io, scratch, buf),
-            F32(io, scratch) => read_more!(io, scratch, buf),
-            F64(io, scratch) => read_more!(io, scratch, buf),
+            I8(io, scratch) => read_more!(io, scratch, T, buf),
+            U8(io, scratch) => read_more!(io, scratch, T, buf),
+            I16(io, scratch) => read_more!(io, scratch, T, buf),
+            U16(io, scratch) => read_more!(io, scratch, T, buf),
+            I32(io, scratch) => read_more!(io, scratch, T, buf),
+            U32(io, scratch) => read_more!(io, scratch, T, buf),
+            F32(io, scratch) => read_more!(io, scratch, T, buf),
+            F64(io, scratch) => read_more!(io, scratch, T, buf),
         }
     }
 }
 
-impl<'a, T: NumCast + Num> Input<'a, T> {
+impl<'a, T: OmniNormal + Num + Clone> Input<'a, T> {
     pub fn read(&mut self) -> alsa::Result<()> {
         self.source.read(&mut self.buf)
     }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,20 @@
+#![warn(clippy::all)]
+
 mod input;
 mod mic;
+mod normalize;
 
+use alsa::pcm::PCM;
+use advanced_collections::circular_buffer::CircularBuffer;
+use nix::Errno;
+use ordered_float::NotNan;
 use rustfft::FFTplanner;
+use rustfft::FFTnum;
 use rustfft::num_complex::Complex;
-use rustfft::num_traits::Zero;
+use rustfft::num_traits::{Float, Num, Zero};
 
 use input::Input;
+use normalize::OmniNormal;
 use mic::{open_microphone, MicSettings};
 
 fn main() -> Result<(), String> {
@@ -15,20 +24,60 @@ fn main() -> Result<(), String> {
     };
     let mic = open_microphone("default", set)
         .map_err(|e| format!("Failed to open the microphone: {}", e))?;
-    let mut input: Input<'_, f64> = Input::from_pcm(&mic, 1024)
+    let mut input: Input<'_, f64> = Input::from_pcm(&mic, 300)
         .map_err(|e| format!("Failed to initialize input: {}", e))?;
-    input.read()
-        .map_err(|e| format!("Failed to read samples: {}", e))?;
-    let mut inbuf = input.get_frames();
-    dbg!(inbuf.len());
-    let mut outbuf = vec![Complex::zero(); inbuf.len()];
     let mut planner = FFTplanner::new(false);
-    let fft = planner.plan_fft(inbuf.len());
-    fft.process(&mut inbuf, &mut outbuf);
-
-    for item in outbuf.iter() {
-        println!("{}", item);
+    let mut old_buckets = CircularBuffer::new(10);
+    loop {
+        let mut inbuf = make_frame_buffer(&mic, &mut input)?;
+        let outbuf = run_fft(&mut planner, &mut inbuf);
+        let max = max_index(&outbuf[1..outbuf.len() / 2], 1e-3);
+        if let Some(max) = max {
+            old_buckets.push_front(max);
+        }
+        if let Some(avg) = average_index(&old_buckets) {
+            eprintln!("{}", avg);
+        }
+        std::thread::sleep(std::time::Duration::from_millis(100));
     }
+}
 
-    Ok(())
+fn make_frame_buffer<'a, T>(mic: &PCM, input: &mut Input<'a, T>) -> Result<Vec<Complex<T>>, String>
+where T: OmniNormal + Num + Clone 
+{
+    if let Err(e) = input.read() {
+        match e.errno() {
+            Some(Errno::EAGAIN) => { },
+            Some(Errno::EPIPE) => mic.try_recover(e, true)
+                .map_err(|e| format!("Failed to recover from a EPIPE error: {}", e))?,
+            Some(_) => return Err(format!("Failed to read input: {}", e)),
+            None => { },
+        }
+    };
+    Ok(input.get_frames())
+}
+
+fn run_fft<T>(planner: &mut FFTplanner<T>, buf: &mut [Complex<T>]) -> Vec<Complex<T>> 
+where T: FFTnum + Clone
+{
+    let mut outbuf = vec![Complex::zero(); buf.len()];
+    let fft = planner.plan_fft(buf.len());
+    fft.process(buf, &mut outbuf);
+    outbuf
+}
+
+fn max_index<T>(buf: &[Complex<T>], threshold: T) -> Option<usize> 
+where T: Float
+{
+    buf.iter()
+        .map(|c| c.re.abs())
+        .enumerate()
+        .flat_map(|(i, f)| NotNan::new(f).map(|nnan| (i, nnan)))
+        .filter(|(_, f)| f.abs() > threshold)
+        .max_by_key(|(_, f)| *f)
+        .map(|(i, _)| i)
+}
+
+fn average_index(buf: &CircularBuffer<usize>) -> Option<usize> {
+    buf.iter().sum::<usize>().checked_div(buf.len())
 }
